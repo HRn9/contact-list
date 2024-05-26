db96112235ca1879e6c1d9d46c76cc5d6effa0d1
Mark Romanov <oxxo8898@gmail.com>
2024-05-27 01:04:25 +0300

    feat: add validation

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,77 +1,96 @@
 import { Grid, TextField, Button } from "@mui/material";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import * as Yup from "yup";
-import { StringSchema } from "yup";
-import useValidationSchema from "../hooks/useValidationSchema";
 
 export default function ContactForm() {
-  const nameRef = useRef(null);
-  const vacancyRef = useRef(null);
-  const phoneRef = useRef(null);
+  const validationSchema = Yup.object({
+    name: Yup.string()
+      .required("Field name is required")
+      .min(3, "Name must be at least 3 characters long"),
+    vacancy: Yup.string().required("Field vacancy is required"),
+    phone: Yup.string()
+      .required("Field phone is required")
+      .matches(/^\d+$/, "Phone must contain only numbers"),
+  });
 
-  const [nameError, setNameError] = useState("");
-  const [vacancyError, setVacancyError] = useState("");
-  const [phoneError, setPhoneError] = useState("");
+  type Errors = Partial<Record<"name" | "vacancy" | "phone", string>>;
 
-  function handleAdding() {
-    const validationSchema = Yup.object().shape({
-      name: Yup.string()
-        .required("Field name is required")
-        .min(3, "Name must be at least 3 characters long"),
-      vacancy: Yup.string().required("Field vacancy is required"),
-      phone: Yup.string()
-        .required("Field phone is required")
-        .matches(/^\d+$/, "Phone must contain only numbers"),
-    });
+  const [errors, setErrors] = useState<Errors>({});
 
-    const validationSchemaFields = validationSchema.fields as unknown as Record<string, StringSchema>;
+  const [name, setName] = useState("");
+  const [vacancy, setVacancy] = useState("");
+  const [phone, setPhone] = useState("");
 
-    const nameValue = (nameRef.current as unknown as HTMLInputElement).value;
-    const vacancyValue = (vacancyRef.current as unknown as HTMLInputElement)
-      .value;
-    const phoneValue = (phoneRef.current as unknown as HTMLInputElement).value;
+  const handleError = (error: Yup.ValidationError) => {
+    const errors = error.inner.reduce((acc, error) => {
+      const { path, message } = error;
+      if (path) {
+        acc[path as keyof Errors] = message;
+      }
+      return acc;
+    }, {} as Errors);
+    setErrors(errors);
+  };
 
-    useValidationSchema(validationSchemaFields.name, nameValue, setNameError);
-    useValidationSchema(validationSchemaFields.vacancy, vacancyValue, setVacancyError);
-    useValidationSchema(validationSchemaFields.phone, phoneValue, setPhoneError);
+  async function handleSubmit() {
+    try {
+      setErrors({});
+
+      await validationSchema.validate(
+        { name, vacancy, phone },
+        { abortEarly: false }
+      );
+    } catch (err) {
+      handleError(err as unknown as Yup.ValidationError);
+    }
   }
+
   return (
-    <Grid container spacing={2} justifyContent="center" alignItems="center">
+    <Grid
+      container
+      spacing={2}
+      justifyContent="center"
+      alignItems="center"
+      marginBottom={Object.keys(errors).length ? '2.5rem' : "0px"}
+    >
       <Grid item xs={2}>
         <TextField
-          error={!!nameError}
+          error={!!errors.name}
           size="small"
           id="outlined-basic"
           label="Name"
-          helperText={nameError}
+          helperText={errors.name}
           variant="outlined"
-          inputRef={nameRef}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
       </Grid>
       <Grid item xs={2}>
         <TextField
-          error={!!vacancyError}
-          helperText={vacancyError}
+          error={!!errors.vacancy}
+          helperText={errors.vacancy}
           size="small"
           id="outlined-basic"
           label="Vacancy"
           variant="outlined"
-          inputRef={vacancyRef}
+          value={vacancy}
+          onChange={(e) => setVacancy(e.target.value)}
         />
       </Grid>
       <Grid item xs={2}>
         <TextField
-          error={!!phoneError}
-          helperText={phoneError}
+          error={!!errors.phone}
+          helperText={errors.phone}
           size="small"
           id="outlined-basic"
           label="Phone"
           variant="outlined"
-          inputRef={phoneRef}
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
         />
       </Grid>
       <Grid item xs={0}>
-        <Button variant="outlined" onClick={handleAdding}>
+        <Button variant="outlined" onClick={handleSubmit}>
           ADD
         </Button>
       </Grid>
